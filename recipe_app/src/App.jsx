32ca209c4b9b3fa41dbad3307cee1cd14e69898a
Aneeta Jose <itsaneetajose@gmail.com>
2024-09-17 21:12:09 +0530

    Adding food details

--- recipe_app/src/App.jsx
+++ recipe_app/src/App.jsx
@@ -4,15 +4,23 @@ import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
 import "./App.css";
 import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
+import FoodDetail from "./components/FoodDetails";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("");
   return (
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <Container>
-        <FoodList foodData={foodData} />
+        <InnerContainer>
+          <FoodList foodData={foodData} setFoodId={setFoodId} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetail foodId={foodId}/>
+        </InnerContainer>
       </Container>
     </div>
   );
---
+++ recipe_app/src/components/FoodDetails.jsx
@@ -0,0 +1,5 @@
+export default function FoodDetail({foodId}){
+    return(<div>
+        Food Details {foodId}
+    </div>)
+}
