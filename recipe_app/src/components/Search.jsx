c15fd9e7114c3a4fe73e9363e47eb63627c5912b
Aneeta Jose <itsaneetajose@gmail.com>
2024-09-17 10:15:30 +0530

    Created a food list component

--- recipe_app/src/components/Search.jsx
+++ recipe_app/src/components/Search.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "3c30508fd66542379bc67ec571be85e4";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
